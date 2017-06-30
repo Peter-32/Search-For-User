66744f55730ee40eaea7098e3bff84873a9ab1ff
Peter-32 <peter@impactradius.com>
2017-06-29 22:52:27 -0700

    second commit

---
+++ js/script.js
@@ -0,0 +1,38 @@
+function print(message) {
+  var outputDiv = document.getElementById("output");
+  outputDiv.innerHTML = message;
+}
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function printStudent(student) {
+html = "<h3>" + student.name + "</h3>"
+"<ul>";
+for (var prop in student) {
+	if (prop != 'name') {
+		html += "<li>" + capitalizeFirstLetter(prop) + ": " + student[prop] + "</li>";
+	}
+}
+html += "</ul>";
+print(html);
+}
+
+var search;
+
+while (true) {
+    search = prompt('What student would you like to search for?  (Type "quit" to exit)');
+    if (search.toUpperCase() === 'QUIT') {
+    	break;
+    }
+    
+    for (var i = 0; i < students.length; i++) {
+    	var student = students[i];
+    	if (student.name.toUpperCase() === search.toUpperCase()) {
+    		printStudent(student)
+    		break;    		
+    	}
+    }
+
+} 
